9be8cfd50b0f670adb232df884b20bbe50cfd7af
khalilelamraoui <khalilelam16@gmail.com>
2023-11-16 05:12:04 -0800

    removed text

--- shell.c
+++ shell.c
@@ -7,7 +7,6 @@ int main(void)
 {
 	char command[MAX_COMMAND_LENGTH];
 
-	printf("============Simple Shell============\n");
 	while (display_prompt(), read_command(command),
 			strcmp(command, "exit") != 0)
 		execute_command(command);

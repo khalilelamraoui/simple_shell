544ac689a46bc8ff347bcb4039a4f169ac81c793
El Amraoui Khalil <khalilelam16@gmail.com>
2023-11-16 23:28:12 +0100

    fixed betty for fisrst file

--- compare_strs.c
+++ compare_strs.c
@@ -2,14 +2,14 @@
 /**
  * Compare two strings.
  */
-int compare_strings(char *string1, char *string2) {
+int compare_strings(char *string1, char *string2)
+{
     int index = 0;
 
     while (string1[index] && string2[index]) {
-        if (string1[index] - string2[index] != 0) {
+        if (string1[index] - string2[index] != 0)
             return string1[index] - string2[index];
-        }
         index++;
     }
-    return 0;
+    return (0);
 }
